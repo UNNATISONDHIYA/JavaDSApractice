6d15617d1204785e267db83dc01fca4a0638aa2d
UNNATISONDHIYA <sondhiyaunnati02@gmail.com>
2025-05-06 13:39:19 +0530

    java

---
+++ ReverseElementInPlace.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+public class ReverseElementInPlace {
+    public static void main(String[] args) {
+    Scanner s = new Scanner(System.in);
+    System.out.println("Enter size of array:");
+    int n = s.nextInt();
+    int[] arr = new int[n];
+    System.out.println("Enter elements:");
+    for(int i=0;i<n;i++)
+    {
+        arr[i] = s.nextInt();
+    }
+    System.out.println("Enter an index from which you want to reverse:");
+    int start = s.nextInt();
+    int end=n-1;
+    if(start>=0 && end<arr.length && start<end)
+    {
+    while(start<end)
+    {
+        int temp=arr[start];
+        arr[start]=arr[end];
+        arr[end]=temp;
+        start++;
+        end--;
+    }
+    System.out.println("Array after reversing from given index");
+    for(int array:arr)
+    {
+       System.out.println(array);
+    }
+}
+else{
+    System.out.println("Invalid Index");
+}
+
+
+}
+}
+
